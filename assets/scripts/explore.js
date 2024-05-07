90d6a077fbd9e52ad0553019a534fce1c0d8b238
fighterkabir <kshergill336@gmail.com>
2024-05-07 04:34:00 -0700

    expose and part of explore complete.

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,70 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+
+  const synth = window.speechSynthesis;
+  const inputTxt = document.getElementById("text-to-speak");
+  var voiceSelect = document.querySelector("select");
+
+  var voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  var speechButton = document.querySelector("button");
+
+  speechButton.addEventListener("click", function() {
+
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+    
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+
+    utterThis.addEventListener("start", function() {
+      var img = document.querySelector("img");
+      img.src = "assets/images/smiling-open.png";
+    });
+
+    utterThis.addEventListener("end", function() {
+      var img = document.querySelector("img");
+      img.src = "assets/images/smiling.png";
+    })
+    
+    if (selectedOption != null) {
+      synth.speak(utterThis);
+    }
+
+  }) 
+  
 }
+
+
+
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,87 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+const audio = document.querySelector("audio");
+const jsConfetti = new JSConfetti();
+
 
 function init() {
-  // TODO
+
+  changeMainImageAndAudio();
+  manipulateVolume();
+  handleButtonPress();
+}
+
+function changeMainImageAndAudio() {
+
+  var dropDownMenu = document.getElementById("horn-select");
+  dropDownMenu.addEventListener("change", function() {
+
+    var selectedHorn = dropDownMenu.value;
+    var image = document.querySelector("#expose > img");
+
+    
+    if (selectedHorn == 'air-horn'){
+
+      image.src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
+
+    } else if (selectedHorn == 'car-horn') {
+
+      image.src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
+
+    } else if (selectedHorn == 'party-horn'){
+
+      image.src = "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
+
+    } else{
+      audio.src = "";
+    }
+
+  });
+}
+
+function manipulateVolume() {
+
+  var volumeSlider = document.getElementById("volume");
+  var audio = document.querySelector("audio");
+
+  volumeSlider.addEventListener("input", function() {
+
+    var userVolumeValue = volumeSlider.value;
+    var volumeIcon = document.querySelector("#volume-controls img");
+    if (userVolumeValue == 0) {
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+      audio.volume = userVolumeValue / 100;
+    } else if (userVolumeValue < 33) {
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+      audio.volume = userVolumeValue / 100;
+    } else if (userVolumeValue < 67) {
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+      audio.volume = userVolumeValue / 100;
+    } else {
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+      audio.volume = userVolumeValue / 100;
+    }
+
+    
+  })
+}
+
+function handleButtonPress() {
+
+  var playButton = document.querySelector("button")
+  playButton.addEventListener("click", function() {
+
+    var currentHorn = document.getElementById("horn-select").value;
+    if (currentHorn == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+
+    console.log(audio.volume);
+    audio.play();
+  })
+  
 }
